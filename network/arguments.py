037534cc52e6f7dc01f7d5045c2554430adcd878
Ivan Anishchanka <aivan@uw.edu>
2020-08-19 03:39:12 -0700

    predict_many.py added

--- network/arguments.py
+++ network/arguments.py
@@ -10,3 +10,15 @@ def get_args():
     args = parser.parse_args()
 
     return args
+
+
+def get_args_many():
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("ALNDIR", type=str, help="folder with input multiple sequence alignments in A3M/FASTA format")
+    parser.add_argument("NPZDIR", type=str, help="folder to save predicted distograms and anglegrams")
+    parser.add_argument('-m', type=str, required=True, dest='MDIR', help='folder with the pre-trained network')
+                
+    args = parser.parse_args()
+                    
+    return args
